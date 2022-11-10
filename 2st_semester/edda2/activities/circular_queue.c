731bc18b5af0297319595b376ee401308526bb35
pedro-rampazo <pedro.rampazo@hotmail.com>
2022-11-10 16:38:01 -0300

    commit 11-10-2022 04:38 p.m.

---
+++ 2st_semester/edda2/activities/circular_queue.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TRUE 0
+#define FALSE 1
+#define MAX 10
+
+int queue_array[MAX];
+
+struct queue{
+    int length;
+    int begin;
+    int end;
+};
+
+struct queue Qinit(){
+    struct queue queue_init;
+    queue_init.length = MAX;
+    queue_init.begin = 0;
+    queue_init.end = -1;
+
+    return queue_init;
+}
+
+int Qisempty(struct queue qarg){
+    if(qarg.end == TRUE){
+        return TRUE;
+    }else{
+        return FALSE;
+    }
+}
+
+int Qisfull(struct queue qarg){
+    if(qarg.end == qarg.length - 1){
+        return TRUE;
+    }else{
+        return FALSE;
+    }
+}
+
+struct queue Enqueue(struct queue qarg, int quantity){
+    srand(time(NULL));
+    for(int i = 0; i < quantity; i++){
+        if(Qisfull(qarg) == TRUE){
+            qarg.end = 0;
+        }else{
+            qarg.end++;
+        }
+        if(qarg.end == qarg.begin){
+            if(qarg.begin == MAX-1){
+                qarg.begin = 0;
+            }
+            else{
+                qarg.begin++;
+            }
+        }
+        queue_array[qarg.end] = rand() % (MAX*10);
+    }
+    return qarg;
+}
+
+struct queue Dequeue(struct queue qarg){
+    if(Qisempty(qarg) == TRUE){
+        printf("EMPTY QUEUE!");
+    }else{
+        printf("%d\n", queue_array[qarg.begin]);
+        qarg.begin++;
+    }
+    return qarg;
+}
+
+printBoolValue(int bool_value){
+    if(bool_value == TRUE){
+        printf("TRUE");
+    }else{
+        printf("FALSE");
+    }
+}
+
+int main(){
+    int opt, qtt;
+    struct queue my_queue = Qinit();
+
+    printf("CIRCULAR QUEUE INITIALIZED.\n");
+    printf("CIRCULAR QUEUE OPTIONS:\n[1] QUEUE IS EMPTY\n[2] QUEUE IS FULL\n[3] ENQUEUE\n[4] DEQUEUE\n[5] PRINT QUEUE\n[0] EXIT\n");
+    scanf("%d", &opt);
+
+    while(opt != 0){
+        switch(opt){
+            case 1:
+                printBoolValue(Qisempty(my_queue));
+                break;
+            case 2:
+                printBoolValue(Qisfull(my_queue));
+                break;
+            case 3:
+                printf("HOW MANY NUMBERS TO ADD? ");
+                scanf("%d", &qtt);
+                my_queue = Enqueue(my_queue, qtt);
+                break;
+            case 4:
+
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+        }
+
+        printf("\n\nCIRCULAR QUEUE OPTIONS:\n[1] QUEUE IS EMPTY\n[2] QUEUE IS FULL\n[3] ENQUEUE\n[4] DEQUEUE\n[5] PRINT QUEUE\n[0] EXIT\n");
+        scanf("%d", &opt);
+    }
+    
+
+    return 0;
+}
--- 2st_semester/edda2/activities/stack.c
+++ 2st_semester/edda2/activities/stack.c
@@ -1,19 +1,29 @@
+/*
+PILHA:
+ESSE ALGORITMO REPRESENTA O FUNCIONAMENTO DE UMA PILHA.
+POSSUI OS CONCEITOS DE FIFO (FIRST-IN FIRST-OUT)
+
+AUTOR: PEDRO MARCOLINO RAMPAZO (SP3100065)
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define MAX 10
 
 int array[MAX];
-int top = 0;
+int top = -1;
 int *p_array;
 
+// FUNÇÃO DE IMPRESSÃO DA PILHA
 int printArray(int *array, int length){
+    printf("[ ");
     for(int i = 0; i < length; i++){
         printf("%d ", array[i]);
     }
-    printf("\n");
+    printf("]\n");
 }
 
+// FUNÇÃO DE ADIÇÃO DE ELEMENTOS NA PILHA
 int push(int quantity_number){
     srand(time(NULL));
     for(int i = 0; i < quantity_number; i++){
@@ -21,23 +31,21 @@ int push(int quantity_number){
             printf("FULL STACK!");
             break;
         }
-        else if(top == 0){
-            array[top] = rand() % (MAX*10);
-            top++;
-        }
         else{
-            array[top+1] = rand() % (MAX*10);
             top++;
+            array[top] = rand() % (MAX*10);
         }
     }
+    printArray(array, top+1);
 }
 
+// FUNÇÃO DE SUBTRAÇÃO DE UM ELEMNTO DA PILHA
 int pop(){
     if(top < 0){
         printf("EMPTY STACK!");
     }
     else{
-        printf("\n%d\n", array[top]);
+        printf("[ %d ]\n", array[top]);
         top--;
     }
 }
@@ -45,30 +53,27 @@ int pop(){
 int main(){
     int opt, qtt;
 
-    printf("\n\nSTACK OPTIONS:\n[1] PUSH\n[2] POP\n[3] PRINT STACK\n[0] EXIT\n");
+    printf("STACK OPTIONS:\n[1] PUSH\n[2] POP\n[3] PRINT STACK\n[0] EXIT\n"); // MENU DE OPÇÕES DA PILHA
     scanf("%d", &opt);
 
     while(opt != 0){
-        
         switch(opt){
-            case 1:
-                printf("HOW MANY NUMBERS TO ADD?\n");
+            case 1: // PRIMEIRA OPÇÃO DO MENU - FUNÇÃO PUSH
+                printf("HOW MANY NUMBERS TO ADD? "); // PERGUNTA AO USUÁRIO QUANTOS ELEMENTO DESEJA ADICIONAR NA PILHA
                 scanf("%d", &qtt);
                 push(qtt);
                 break;
-            case 2:
+            case 2: // SEGUNDA OPÇÃO DO MENU - FUNÇÃO POP (ELIMINA APENAS UM ELEMENTO POR VEZ)
                 pop();
                 break;
-            case 3:
-                printArray(array, top);
+            case 3: // TERCEIRA OPÇÃO DO MENU - IMPRESSÃO DA PILHA
+                printArray(array, top+1);
                 break;
             default:
                 printf("INVALID OPTION.");
         }
-
-        printf("STACK OPTIONS:\n[1] PUSH\n[2] POP\n[3] PRINT STACK\n[0] EXIT\n");
+        printf("\n\nSTACK OPTIONS:\n[1] PUSH\n[2] POP\n[3] PRINT STACK\n[0] EXIT\n"); // MENU DE OPÇÕES DA PILHA
         scanf("%d", &opt);
     }
-
     return 0;
 }
