6b1513015d0a89466c20c39065852f6cff067f18
pedro-rampazo <pedro.rampazo@hotmail.com>
2022-11-21 15:35:09 -0300

    commit 11-21-2022 03:35 p.m.

--- 2st_semester/edda2/activities/dynamic_tree.c
+++ 2st_semester/edda2/activities/dynamic_tree.c
@@ -17,29 +17,56 @@ treeNode *createTree(int root_value){
     return root;
 }
 
-void addNode(treeNode *tree, int node_value){
-    treeNode *new_node = malloc(sizeof(treeNode));
-    new_node -> value = node_value;
-    if(tree -> value <= node_value){
+treeNode * addNode(treeNode *tree, int node_value){
+    if(tree -> value >= node_value){
         if(tree -> left){
-            addNode(tree -> left, new_node -> value);
+            addNode(tree -> left, node_value);
         }
         else{
+            treeNode *new_node = malloc(sizeof(treeNode));
+            new_node -> value = node_value;
+            new_node -> father = tree;
             tree -> left = new_node;
+            return tree;
         }
     }else{
         if(tree -> right){
-            addNode(tree -> right, new_node -> value);
+            addNode(tree -> right, node_value);
         }
         else{
+            treeNode *new_node = malloc(sizeof(treeNode));
+            new_node -> value = node_value;
+            new_node -> father = tree;
             tree -> right = new_node;
+            return tree;
         }
     }
+}
+// RETORNAR VALOR
 
+void preOrder(treeNode *tree){
+    printf("%d ", tree -> value);
+    if(tree -> left){
+        preOrder(tree -> left);
+    }
+    if(tree -> right){
+        preOrder(tree -> right);
+    }
 }
 
+
 int main(){
-    treeNode *my_tree = createTree(5);
+    int tree_number[] = {56, 87, 60, 36, 4, 59, 94, 10, 43, 40};
+
+    treeNode *my_tree = createTree(tree_number[0]);
+
+    my_tree = addNode(my_tree, 34);
+    my_tree = addNode(my_tree, 60);
+    my_tree = addNode(my_tree, 58);
+    my_tree = addNode(my_tree, 62);
+
+    preOrder(my_tree);
+    
 
 
     return 0;
