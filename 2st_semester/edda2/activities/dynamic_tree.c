33b1fb106a268c45af16bd387c1d7a00128f889b
pedro-rampazo <pedro.rampazo@hotmail.com>
2022-11-22 10:53:03 -0300

    commit 11-22-2022 10:53 a.m.

--- 2st_semester/edda2/activities/dynamic_tree.c
+++ 2st_semester/edda2/activities/dynamic_tree.c
@@ -1,51 +1,59 @@
+/*
+ÁRVORE BINÁRIA DINÂMICA:
+- ESSE ALGORITMO REPRESENTA UMA ÁRVORE BINÁRIA COM ALOCAÇÃO DINÂMICA RECURSIVA.
+- O USUÁRIO INFORMA O NÚMERO DE NÓS PARA A MONTAGEM DA ÁRVORE.
+- A ÁRVORE É CONSTRUÍDA PELA ORDEM DE SORTEIO DOS NÚMEROS, ONDE SERÃO ADICIONADOS A ESQUERDA,
+QUANDO O NÓ-PAI FOR MAIOR QUE O VALOR, OU A DIREITA, QUANDO O NÓ-PAI FOR MENOR DO QUE O VALOR.
+
+AUTOR: PEDRO MARCOLINO RAMPAZO (SP3100065)
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+// ESTRUTURA DO NÓ DA ÁRVORE
 typedef struct treeNode{
     int value;
     struct treeNode *left;
     struct treeNode *right;
-    struct treeNode *father;
 }treeNode;
 
+// CRIAÇÃO DA ÁRVORE, RETORNANDO A RAIZ
 treeNode *createTree(int root_value){
     treeNode *root = malloc(sizeof(treeNode));
     root -> value = root_value;
     root -> left = NULL;
     root -> right = NULL;
-    root -> father = NULL;
     return root;
 }
 
+// ADIÇÃO DO NÓ
 treeNode * addNode(treeNode *tree, int node_value){
     if(tree -> value >= node_value){
         if(tree -> left){
-            addNode(tree -> left, node_value);
+            tree -> left = addNode(tree -> left, node_value);
         }
         else{
             treeNode *new_node = malloc(sizeof(treeNode));
             new_node -> value = node_value;
-            new_node -> father = tree;
             tree -> left = new_node;
-            return tree;
         }
     }else{
         if(tree -> right){
-            addNode(tree -> right, node_value);
+            tree -> right = addNode(tree -> right, node_value);
         }
         else{
             treeNode *new_node = malloc(sizeof(treeNode));
             new_node -> value = node_value;
-            new_node -> father = tree;
             tree -> right = new_node;
-            return tree;
         }
     }
+    return tree;
 }
-// RETORNAR VALOR
 
+// PERCORRIMENTO DA ÁRVORE EM PRÉ-ORDEM
 void preOrder(treeNode *tree){
-    printf("%d ", tree -> value);
+    printf("[%d] ", tree -> value);
     if(tree -> left){
         preOrder(tree -> left);
     }
@@ -54,20 +62,51 @@ void preOrder(treeNode *tree){
     }
 }
 
+// PERCORRIMENTO DA ÁRVORE EM-ORDEM
+void inOrder(treeNode *tree){
+    if(tree -> left){
+        inOrder(tree -> left);
+    }
+    printf("[%d] ", tree -> value);
+    if(tree -> right){
+        inOrder(tree -> right);
+    }
+}
+
+// PERCORRIMENTO DA ÁRVORE EM PÓS-ORDEM
+void postOrder(treeNode *tree){
+    if(tree -> left){
+        postOrder(tree -> left);
+    }
+    if(tree -> right){
+        postOrder(tree -> right);
+    }
+    printf("[%d] ", tree -> value);
+}
 
 int main(){
-    int tree_number[] = {56, 87, 60, 36, 4, 59, 94, 10, 43, 40};
+    int number_nodes, rand_node;
 
-    treeNode *my_tree = createTree(tree_number[0]);
+    srand(time(NULL));
+    treeNode *my_tree = createTree(rand() % 100);
 
-    my_tree = addNode(my_tree, 34);
-    my_tree = addNode(my_tree, 60);
-    my_tree = addNode(my_tree, 58);
-    my_tree = addNode(my_tree, 62);
+    printf("TREE INITIALIZED!\nNUMBER OF NODES: ");
+    scanf("%d", &number_nodes);
 
+    printf("\n%d ", my_tree -> value);
+
+    for(int i = 1; i < number_nodes; i++){
+        rand_node = rand() % 100;
+        my_tree = addNode(my_tree, rand_node);
+        printf("%d ", rand_node);
+    }
+
+    printf("\nPREORDER:\n");
     preOrder(my_tree);
-    
-
+    printf("\nINORDER:\n");
+    inOrder(my_tree);
+    printf("\nPOSTORDER:\n");
+    postOrder(my_tree);
 
     return 0;
 }
