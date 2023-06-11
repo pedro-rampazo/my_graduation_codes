200c42c781f38a023218089b9c289d698cc20456
pedro-rampazo <pedro.rampazo@hotmail.com>
2023-06-11 04:06:44 -0300

    dw1a3 - continuando projeto golden piggy

--- 3st_semester/dw1a3/project/golden_piggy/js/player_script.js
+++ 3st_semester/dw1a3/project/golden_piggy/js/player_script.js
@@ -22,11 +22,47 @@ const Modal = {
         document.getElementById('point-label').textContent = obj.points
         document.getElementById('hit-label').textContent = obj.hits
         document.getElementById('close-icon').onclick = restoreModal
+        
+        document.getElementById('save-bttn').onclick = function () {
+            Modal.updateConfirmation(index)
+        }
+        
+        document.getElementById('delete-bttn').onclick = function() {
+            Modal.removeConfirmation(index)
+        }
 
         Modal.open('#pl-modal-overlay')
     },
-    confirmationValue(){
+    removeConfirmation(index){
+        var yesBttn = document.getElementById('yes-bttn')
+        var noBttn = document.getElementById('no-bttn')
+        Modal.open('#confirmation-modal-overlay')
+        yesBttn.onclick = function() {
+            Player.remove(index)
+            Modal.close('#confirmation-modal-overlay')
+            Modal.close('#pl-modal-overlay')
+            App.reload()
+        }
+        noBttn.onclick = function() {
+            Modal.close('#confirmation-modal-overlay')
+        }
+    },
+    updateConfirmation(index){
+        var yesBttn = document.getElementById('yes-bttn')
+        var noBttn = document.getElementById('no-bttn')
         
+        yesBttn.onclick = function() {
+            Player.update(index)
+            Modal.close('#confirmation-modal-overlay')
+            Modal.close('#pl-modal-overlay')
+            App.reload()
+        }
+
+        noBttn.onclick = function() {
+            Modal.close('#confirmation-modal-overlay')   
+        }
+
+        Modal.open('#confirmation-modal-overlay')
     },
 }
 
@@ -44,8 +80,7 @@ const Behavior = {
         let obj = Player.all[index]
         
         document.getElementById('player-name-inpt').value = obj.name
-        document.getElementById('player-name-inpt').disabled = true
-        
+        document.getElementById('player-name-inpt').disabled = true   
     }
 }
 
@@ -62,22 +97,17 @@ const DOM = {
     mainContainer: document.querySelector('#pl-section'),
 
     addPlayer(player, index) {
-        
         function openModal() {
             Modal.openEdit(index)
         }
-        
         const sec = document.createElement('section')
         sec.classList.add('player')
         sec.innerHTML = `<h4>${player.name}</h4>
                         <img src="../images/arrow_icon.svg" alt="View">`
         sec.dataset.index = index
-
         sec.onclick = openModal
-
         DOM.mainContainer.appendChild(sec)
     },
-
     clearPlayers() {
         DOM.mainContainer.innerHTML = ''
     }
@@ -89,21 +119,25 @@ const Player = {
     add() {
         const nwPlNmInput = document.getElementById('nwplnm-input').value
         let {name, points, hits, position} = {name: nwPlNmInput, points: 0, hits: 0, position: null}
-
         Player.all.push({name, points, hits, position})
+        Behavior.restoreNwPlModalOverlay()
         App.reload()
-    }
+    },
+    remove(index){
+        Player.all.splice(index, 1)
+    },
+    update(index){
+        const newValue = document.getElementById('player-name-inpt').value
+        Player.all[index].name = newValue
+    },
 }
 
 const App = {
     init() {
         Player.all.forEach(DOM.addPlayer)
-
         Storage.set(Player.all)
     },
     reload() {
-        document.getElementById('nwplnm-input').value = ''
-        Modal.close('#nwpl-modal-overlay')
         DOM.clearPlayers()
         App.init()
     }
